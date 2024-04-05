cda0078d560edda75e68e85c81e8ce7b7495e86e
vedant144064 <130687835+vedant144064@users.noreply.github.com>
2024-04-05 23:36:00 +0530

    Vedant updates

---
+++ cleanarr.js
@@ -0,0 +1,200 @@
+const members = [
+  [
+    {
+      Name: "Kunj Panchal",
+      Email: "ep21btech11017@iith.ac.in",
+      Photo:
+        "images/WhatsApp Image 2024-02-16 at 12.57.26 AM - Kunj Panchal.jpeg",
+      Designation: "Overall Head",
+    },
+    {
+      Name: "Pragya Yadav",
+      Email: "ch21btech11023@iith.ac.in",
+      Photo: "images/Website - Cepheid Astronomy club.jpg",
+      Designation: "Inbound Head",
+    },
+    {
+      Name: "Aman Bhasin",
+      Email: "es21btech11006@iith.ac.in",
+      Photo: "images/IMG_20240209_133819 - Aman Bhasin.jpg",
+      Designation: "Head - Event Domain",
+    },
+    {
+      Name: "Abhijit Kasle ",
+      Email: "ep21btech11002@iith.ac.in",
+      Photo: "images/ep21btech11002_photo - Abhijit Sambhaji Kasle.jpg",
+      Designation: "Media Head",
+    },
+  ],
+  [
+    {
+      Name: "Ambica Govind ",
+      Email: "ep21btech11007@iith.ac.in",
+      Photo: "images/IMG-20240209-WA0085 - Ambica Govind.jpg",
+      Designation: "Mentor",
+    },
+    {
+      Name: "Rushikesh Nilapalle ",
+      Email: "es20btech11022@iith.ac.in",
+      Photo: "images/IMG-20230703-WA0015(1) - RUSHIKESH NILAPALLE.jpg",
+      Designation: "Mentor",
+    },
+  ],
+  [
+    {
+      Name: "Rajiv Shailesh Chitale",
+      Email: "cs21btech11051@iith.ac.in",
+      Photo: "images/IMG-20231224-WA0012 - Rajiv Shailesh Chitale.jpg",
+      Designation: "Senior Core",
+    },
+    {
+      Name: "Ansh Vardhan",
+      Email: "es21btech11008@iith.ac.in",
+      Photo:
+        "images/WhatsApp Image 2023-08-29 at 21.44.37_5d474243 - Ansh Vardhan.jpg",
+      Designation: "Senior Core",
+    },
+    {
+      Name: "Kartik Agrawal",
+      Email: "ee21btech11030@iith.ac.in",
+      Photo: "images/Kartik_Agrawal - Kartik Agrawal.jpg",
+      Designation: "Senior Core",
+    },
+  ],
+  [
+    {
+      Name: "Advait Jain",
+      Email: "cs22btech11003@iith.ac.in",
+      Photo:"images/advait_jain.jpg",
+      Designation: "Inbound",
+    },
+    {
+      Name: "Om Dave",
+      Email: "co22btech11006@iith.ac.in",
+      Photo: "images/IMG-20230609-WA0004 - Om Gopesh Dave.jpg",
+      Designation: "Events",
+    },
+    {
+      Name: "Ayush Jaiswal ",
+      Email: "ce22btech11007@iith.ac.in",
+      Photo: "images/IMG_20240209_002515 - Ayush Jaiswal.jpg",
+      Designation: "Events",
+    },
+    {
+      Name: "Yash",
+      Email: "me22btech11060@iith.ac.in",
+      Photo: "images/IMG_20240209_130401 - Yash choudhary.jpg",
+      Designation: "Inbound",
+    },
+    {
+      Name: "Shreya Sridhar",
+      Email: "ic22btech11015@iith.ac.in",
+      Photo: "images/20240204_164405 - Shreya Sridhar.jpg",
+      Designation: "Inbound",
+    },
+    {
+      Name: "Yasir Usmani",
+      Email: "ai22btech11031@iith.ac.in",
+      Photo: "images/E cell - Yasir Usmani.jpg",
+      Designation: "Inbound",
+    },
+    {
+      Name: "Swapnil Bag",
+      Email: "es22btech11034@iith.ac.in",
+      Photo: "images/20240209_165424 - Swapnil Bag.jpg",
+      Designation: "Events",
+    },
+    {
+      Name: "Kunwar dushyant singh ",
+      Email: "ee22btech11031@iith.ac.in ",
+      Photo: "images/IMG-20230108-WA0020 - Kunwar Dushyant Singh.jpg",
+      Designation: "Inbound",
+    },
+    {
+      Name: "Ome Bharath J",
+      Email: "Ep22btech11019@iith.ac.in",
+      Photo: "images/IMG-20230417-WA0006 - Ome Bharath Jagarlapudi.jpg",
+      Designation: "Inbound",
+    },
+    {
+      Name: "Varun Srivastava",
+      Email: "ch22btech11038@iith.ac.in",
+      Photo: "images/IMG_20240210_233032 - Varun Srivastava.jpg",
+      Designation: "Events",
+    },
+    {
+      Name: "Mehwish Naz",
+      Email: "ch22btech11021@gmail.com",
+      Photo: "images/WhatsApp Image 2023-05-06 at 19.27.01 - Mehwish Naz.jpg",
+      Designation: "Events",
+    },
+    {
+      Name: "Paban shinde ",
+      Email: "co22btech11008@iith.ac.in",
+      Photo: "images/IMG_20240110_230629 - Pavankumar Maharudra Shinde.jpg",
+      Designation: "Inbound",
+    },
+    {
+      Name: "Vyaas",
+      Email: "ep22btech11031@iith.ac.in",
+      Photo: "images/Vyaas Ramakrishnan - Vyaas R.jpeg",
+      Designation: "Media & PR",
+    },
+    {
+      Name: "Shriram H",
+      Email: "me22btech11023@iith.ac.in",
+      Photo:
+        "images/Screenshot_2024-02-12-00-29-48-86_92460851df6f172a4592fca41cc2d2e6 - Shriram H.jpg",
+      Designation: "Media & PR",
+    },
+    {
+      Name: "Vaishnavi Vyasraj",
+      Email: "ep22btech11029@iith.ac.in",
+      Photo: "images/pic2 - Vaishnavi Vyasraj.jpeg",
+      Designation: "Events",
+    },
+    {
+      Name: "Ashish Bhardwaj",
+      Email: "ms22btech11007@iith.ac.in",
+      Photo: "images/IMG-20230609-WA0007 - Ashish Bhardwaj.jpg",
+      Designation: "Events",
+    },
+    {
+      Name: "Harman Singh ",
+      Email: "me22btech11024@iith.ac.in",
+      Photo: "images/IMG_20240212_151059 - Harman Singh.jpg",
+      Designation: "Inbound",
+    },
+    {
+      Name: "Sameer kendal",
+      Email: "ee22btech11044@iith.ac.in",
+      Photo: "images/IMG-20240126-WA0173 - Sameer Kendal.jpg",
+      Designation: "Inbound",
+    },
+    {
+      Name: "Thameem Rasheed ",
+      Email: "bd22bdes11018@iith.ac.in",
+      Photo: "images/me - Thameem Rasheed.JPG",
+      Designation: "Inbound",
+    },
+    {
+      Name: "A.Padmaprabhan",
+      Email: "me22btech11001@iith.ac.in",
+      Photo:
+        "images/PXL_20230703_103815265.PORTRAIT~3 (1) - A Padmaprabhan.jpg",
+      Designation: "Media & PR",
+    },
+    {
+      Name: "Piyush Kothawade",
+      Email: "es22btech11026@iith.ac.in",
+      Photo: "images/MyPic - Piyush Shyam Kothawade.jpg",
+      Designation: "Events",
+    },
+    {
+      Name: "Devansh Jain",
+      Email: "ee22btech11018@iith.ac.in",
+      Photo: "images/Photo - Devansh Jain.jpg",
+      Designation: "Events",
+    },
+  ],
+];
